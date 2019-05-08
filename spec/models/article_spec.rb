09c181545be71473f4afb9bdffd92b6d7f803eef
olgasolov <solo_o_e@mail.ru>
2019-05-08 19:00:51 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,14 @@ describe Article  do
 	describe "associations" do
 		it { should have_many :comments }
 	end	
+
+	describe "#subject" do
+		it "returns the article title" do
+			# создаем  объект article  
+			article = create(:article, title: 'Lorem Ipsum')
+
+			# assert, проверка
+			expect(article.subject).to eq 'Lorem Ipsum'
+		end
+	end
 end
